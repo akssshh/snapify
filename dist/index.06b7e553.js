0edb0f66be17365e844df1d193662ae2843ce1cf
Akash Gogoi <gogoiakash09@gmail.com>
2024-01-28 23:14:47 +0530

    add sticky navbar

--- dist/index.06b7e553.js
+++ dist/index.06b7e553.js
@@ -1,10 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navbar = document.querySelector(".navbar");
-    const mainContent = document.getElementById("main-content");
-    window.addEventListener("scroll", function() {
-        if (window.scrollY > mainContent.offsetTop) navbar.classList.add("scroll");
-        else navbar.classList.remove("scroll");
-    });
-});
 
 //# sourceMappingURL=index.06b7e553.js.map
