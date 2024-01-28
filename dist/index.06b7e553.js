4c9d6246958a0239184fef0168b609538c6729b6
Akash Gogoi <gogoiakash09@gmail.com>
2024-01-28 23:44:42 +0530

    made small ui change

--- dist/index.06b7e553.js
+++ dist/index.06b7e553.js
@@ -1,2 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.getElementById("nav");
+    const mainContent = document.getElementById("main-content");
+    window.addEventListener("scroll", function() {
+        if (window.scrollY >= mainContent.offsetTop) navbar.classList.add("scroll");
+        else navbar.classList.remove("scroll");
+    });
+});
 
 //# sourceMappingURL=index.06b7e553.js.map
---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.getElementById('nav');
+    const mainContent = document.getElementById('main-content');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY >= mainContent.offsetTop) {
+            navbar.classList.add('scroll')
+        } else {
+            navbar.classList.remove('scroll')
+        }
+    })
+})
