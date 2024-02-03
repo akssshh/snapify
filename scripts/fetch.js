b43a2e45069c09b907cca655aa4e695fe36a0779
Akash Gogoi <gogoiakash09@gmail.com>
2024-02-03 15:43:11 +0530

    fix masonry layout bug

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,29 +1,81 @@
-// import Masonry from "masonry-layout";
+let currentPage = 1;
+let isFetching = false;
+let hasMore = true;
 
-const API_KEY = "12gZb1rLsA7rqFgAOuzVaj9Tyi1vpCmsoz1SLzgm_Os";
+const grid = document.querySelector(".grid");
 
-const API_URL = `https://api.unsplash.com/photos?client_id=${API_KEY}&per_page=50`;
-const searchResluts = document.getElementById('main-content');
+const macyInstance = Macy({
+  container: grid,
+  breakAt: {
+    1600: 5,
+    1200: 4,
+    900: 3,
+    600: 2,
+    400: 1,
+  },
+  margin: {
+    x: 15,
+    y: 15,
+  },
+});
 
-async function fetchPhotos() {
-    
-  const res = await fetch(API_URL);
-  const data = await res.json();
-  
-  data.map((result) => {
-    const imageWrapper = document.createElement('div')
-    imageWrapper.classList.add("box")
-    imageWrapper.classList.add("grid-item")
-    const image = document.createElement('img')
-    image.src = result.urls.small;
-    image.alt = result.alt_description
+const fixStartUpBug = () => {
+  macyInstance.runOnImageLoad(function () {
+    macyInstance.recalculate(true, true);
+    var evt = document.createEvent("UIEvents");
+    evt.initUIEvent("resize", true, false, window, 0);
+    window.dispatchEvent(evt);
+  }, true);
+};
 
-    imageWrapper.appendChild(image);
-    searchResluts.appendChild(imageWrapper);
-  })
+const fetchData = async () => {
+  if (isFetching || !hasMore) {
+    return;
+  }
 
+  isFetching = true;
 
-}
-fetchPhotos(API_URL);
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos?client_id=12gZb1rLsA7rqFgAOuzVaj9Tyi1vpCmsoz1SLzgm_Os&page=${currentPage}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.length === 0) {
+      hasMore = false;
+      return;
+    }
+
+    for (let post of data) {
+      let div = document.createElement("div");
+      let img = document.createElement("img");
+      img.src = post.urls.small;
+
+      div.append(img);
+      grid.append(div);
+    }
+    currentPage++;
+    fixStartUpBug();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  } finally {
+    isFetching = false;
+  }
+};
+
+fetchData();
+
+const handleScroll = () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    fetchData();
+  }
+};
+
+window.addEventListener("scroll", handleScroll);
 
 
