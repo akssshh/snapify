38d06d16a33f81417d887ff7830a35f74d788a96
Akash Gogoi <gogoiakash09@gmail.com>
2024-02-05 01:58:51 +0530

    search functionality and infinite scroll

--- scripts/searchFetch.js
+++ scripts/searchFetch.js
@@ -5,19 +5,19 @@ let hasAny = true;
 const grid = document.querySelector(".grid");
 
 const macInstnce = Macy({
-    container: grid,
-    breakAt: {
-      1600: 5,
-      1200: 4,
-      900: 3,
-      600: 2,
-      400: 1,
-    },
-    margin: {
-      x: 15,
-      y: 15,
-    },
-  });
+  container: grid,
+  breakAt: {
+    1600: 5,
+    1200: 4,
+    900: 3,
+    600: 2,
+    400: 1,
+  },
+  margin: {
+    x: 15,
+    y: 15,
+  },
+});
 
 const fixUpBug = () => {
   macInstnce.runOnImageLoad(function () {
@@ -28,20 +28,23 @@ const fixUpBug = () => {
   }, true);
 };
 
-
-function search(event){
-  event.preventDefault(); // Prevents form submission
+function search(event) {
+  event.preventDefault();
   const value = document.getElementById("search-input").value;
   console.log(value);
   return value;
 }
 
-
 async function searchFetchData(event) {
+  if (isFetch || !hasAny) {
+    return;
+  }
+
+  isFetch = true;
   const value = search(event);
   try {
     const response = await fetch(
-      `https://api.unsplash.com/search/photos?client_id=12gZb1rLsA7rqFgAOuzVaj9Tyi1vpCmsoz1SLzgm_Os&page=1&query=${value}`
+      `https://api.unsplash.com/search/photos?client_id=12gZb1rLsA7rqFgAOuzVaj9Tyi1vpCmsoz1SLzgm_Os&page=${currPage}&query=${value}`
     );
 
     if (!response.ok) {
@@ -49,32 +52,36 @@ async function searchFetchData(event) {
     }
 
     const data = await response.json();
-    console.log(data.results)
 
     if (data.results.length === 0) {
       hasAny = false;
       return;
     }
 
-    data.results.forEach(post => {
-    let div = document.createElement("div");
-    let img = document.createElement("img");
-    img.src = post.urls.small;
-    div.append(img);
-    grid.append(div); 
-  });
+    data.results.forEach((post) => {
+      let div = document.createElement("div");
+      let img = document.createElement("img");
+      img.src = post.urls.small;
+      div.append(img);
+      grid.append(div);
+    });
 
-  currPage++;
-  fixUpBug();
-    
+    currPage++;
+    fixUpBug();
   } catch (error) {
     console.error("Error fetching data:", error);
-  } 
+  } finally {
+    isFetch = false;
+  }
+}
+
+const handleScroll = (event) => {
+  console.log("Scroll event fired!");
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    console.log("Calling searchFetchData...");
+    searchFetchData(event)
+  }
 };
 
 
-
-
-
-
-
+window.addEventListener("scroll", handleScroll);
