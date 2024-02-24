875eac7fa305399d2106a5e699fce7080d2219a5
Akash Gogoi <gogoiakash09@gmail.com>
2024-02-24 23:45:48 +0530

    fetching slider data

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,7 @@
 const imageWrapper = document.querySelector(".grid");
 const searchInput = document.querySelector("#search-input");
-const floatSearchInput = document.getElementById('float-search-input');
+const floatSearchInput = document.getElementById("float-search-input");
 const searchButton = document.querySelector("#search-button");
-
-
-
 
 const apiKey = "12gZb1rLsA7rqFgAOuzVaj9Tyi1vpCmsoz1SLzgm_Os";
 let currentPage = 1;
@@ -36,21 +33,88 @@ const fixStartUpBug = () => {
   }, true);
 };
 
+// function to display slider
+
+const sliderContainer = document.getElementById("slider-container");
+
+const titles = [
+  "Cool Tones",
+  "Wallpapers",
+  "Nature",
+  "3D Renders",
+  "Travel",
+  "Architecture & Interiors",
+  "Textures & Patterns",
+  "Street Photography",
+  "Film",
+  "Archival",
+  "Experimental",
+  "Animals",
+  "Fashion & Beauty",
+  "People",
+  "Spirituality",
+  "Business & Work",
+  "Food & Drink",
+  "Health & Wellness",
+  "Sports",
+  "Current Events",
+];
+
+// Function to display the slides
+function displaySlides() {
+  // Clear the slider container
+  sliderContainer.innerHTML = "";
+
+  // Iterate over the names array and create a slide for each
+  titles.forEach((title) => {
+    const slide = document.createElement("div");
+    slide.classList.add("slide");
+    const heading = document.createElement("h4");
+    heading.textContent = title;
+
+    slide.addEventListener("click", () => {
+      currentPage = 1;
+      loadImages(title);
+    });
+
+    slide.appendChild(heading);
+    sliderContainer.appendChild(slide);
+  });
+}
+
+// Call the displaySlides function to initially display the slides
+displaySlides();
+
+function prev() {
+  document.getElementById("slider-container").scrollLeft -= 270;
+}
+
+function next() {
+  document.getElementById("slider-container").scrollLeft += 270;
+}
+
+// function to download image
+
 const downloadImg = (imgUrl) => {
-  fetch(imgUrl).then(res => res.blob()).then(blob => {
+  fetch(imgUrl)
+    .then((res) => res.blob())
+    .then((blob) => {
       const a = document.createElement("a");
       a.href = URL.createObjectURL(blob);
       a.download = new Date().getTime();
       a.click();
-  }).catch(() => alert("Failed to download image!"));
-}
+    })
+    .catch(() => alert("Failed to download image!"));
+};
 
 const generateHTML = (images) => {
+  console.log(images)
   if (currentPage === 1) {
     imageWrapper.innerHTML = "";
   }
 
   imageWrapper.innerHTML += images
+  
     .map(
       (img) =>
         `
@@ -105,13 +169,14 @@ const getImages = async (apiURL) => {
   }
 };
 
-const loadImages = () => {
-  const searchTerm = searchInput.value || floatSearchInput.value;
+const loadImages = (title) => {
+  const searchTerm = title || searchInput.value || floatSearchInput.value;
   let apiUrl = `https://api.unsplash.com/photos?client_id=${apiKey}&page=${currentPage}`;
   apiUrl = searchTerm
     ? `https://api.unsplash.com/search/photos?client_id=${apiKey}&page=${currentPage}&query=${searchTerm}`
     : apiUrl;
   getImages(apiUrl);
+  // currentPage++;
 };
 
 const loadSearchImages = (e) => {
@@ -125,7 +190,7 @@ const loadSearchImages = (e) => {
     currentPage = 1;
     searchTerm = e.target.value;
     imageWrapper.innerHTML = "";
-    loadImages();
+    loadImages(searchTerm);
   }
 };
 
@@ -139,12 +204,15 @@ searchButton.addEventListener("click", () => {
 
 loadImages();
 
-
 const handleScroll = () => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-    loadImages();
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+    searchTerm // Ensure searchTerm is not null
+  ) {
+    loadImages(); // Fetch more images based on the current search term
     currentPage++;
   }
 };
 
 window.addEventListener("scroll", handleScroll);
+
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,27 +1,106 @@
 // Get the slide container and the array of car names
-const sliderContainer = document.getElementById('slider-container');
+const sliderContainer = document.getElementById("slider-container");
 
-
-const titles = ['Cool Tones', 'Wallpapers', 'Nature', '3D Renders', 'Travel', 'Architecture & Interiors', 'Textures & Patterns', 'Street Photography', 'Film', 'Archival', 'Experimental', 'Animals', 'Fashion & Beauty', 'People', 'Spirituality', 'Business & Work', 'Food & Drink', 'Health & Wellness', 'Sports', 'Current Events'];
-
+const titles = [
+  "Cool Tones",
+  "Wallpapers",
+  "Nature",
+  "3D Renders",
+  "Travel",
+  "Architecture & Interiors",
+  "Textures & Patterns",
+  "Street Photography",
+  "Film",
+  "Archival",
+  "Experimental",
+  "Animals",
+  "Fashion & Beauty",
+  "People",
+  "Spirituality",
+  "Business & Work",
+  "Food & Drink",
+  "Health & Wellness",
+  "Sports",
+  "Current Events",
+];
 
 // Function to display the slides
 function displaySlides() {
   // Clear the slider container
-  sliderContainer.innerHTML = '';
+  sliderContainer.innerHTML = "";
 
   // Iterate over the names array and create a slide for each
   titles.forEach((title) => {
-    const slide = document.createElement('div');
-    slide.classList.add('slide');
-    const heading = document.createElement('h4');
+    const slide = document.createElement("div");
+    slide.classList.add("slide");
+    const heading = document.createElement("h4");
     heading.textContent = title;
 
-    slide.addEventListener('click', () => {
+    slide.addEventListener("click", () => {
+      const imageWrapper = document.querySelector(".grid");
       const apiKey = "12gZb1rLsA7rqFgAOuzVaj9Tyi1vpCmsoz1SLzgm_Os";
+      let currentPage = 1;
+      // let isFetching = false;
+      // let hasMore = true;
+      let searchTerm = title;
+
+      const macyInstance = Macy({
+        container: imageWrapper,
+        breakAt: {
+          1600: 5,
+          1200: 4,
+          900: 3,
+          600: 2,
+          400: 1,
+        },
+        margin: {
+          x: 15,
+          y: 15,
+        },
+      });
+
+      const fixStartUpBug = () => {
+        macyInstance.runOnImageLoad(function () {
+          macyInstance.recalculate(true, true);
+          var evt = document.createEvent("UIEvents");
+          evt.initUIEvent("resize", true, false, window, 0);
+          window.dispatchEvent(evt);
+        }, true);
+      };
       // Construct the API query with the clicked title
-      const searchTerm = title;
-      const apiUrl = `https://api.unsplash.com/search/collections?client_id=${apiKey}&page=1&query=${searchTerm}`;
+
+      const generateHTML = (images) => {
+        console.log(images);
+        imageWrapper.innerHTML = "";
+        imageWrapper.innerHTML += images
+          .map(
+            (img) => `
+              <div class="grid-item card" >
+                <img src="${img.urls.small}" class="fetch-img" />
+                <div class="details">
+                  <div class="icons top-icons">
+                    <button class="share-btn">
+                      <i class="uil uil-share-alt"></i>
+                     </button>
+                   </div>
+                <div class="photographer">
+                    <i class="uil uil-camera"></i>
+                    <span>${img.user.name}</span>
+                </div>
+                <div class="">
+                <button class="download-btn" onclick="downloadImg('${img.urls.small}');" >
+                  <i class="uil uil-import"></i>
+                </button>
+              </div>
+            </div>
+        </div>
+            `
+          )
+          .join("");
+        fixStartUpBug();
+      };
+
+      const apiUrl = `https://api.unsplash.com/search/photos?client_id=${apiKey}&page=${currentPage}&query=${searchTerm}`;
 
       // Call a function to fetch and display images based on the selected term
       const getImages = async (apiURL) => {
@@ -29,16 +108,16 @@ function displaySlides() {
           const response = await fetch(apiURL, {
             headers: { Authorization: `Client-ID ${apiKey}` },
           });
-      
+
           if (!response.ok) {
             throw new Error("Failed to load images!");
           }
-      
+
           const data = await response.json();
-          console.log(data);
-      
+          // console.log(data);
+
           let photos = [];
-      
+
           if (data.results) {
             photos = data.results;
           } else {
@@ -49,40 +128,17 @@ function displaySlides() {
           console.error(error);
         }
       };
+
       getImages(apiUrl);
 
-      const imageWrapper = document.querySelector(".grid");
-
-      const generateHTML = (images) => {
-        
-        imageWrapper.innerHTML += images
-          .map(
-            (img) =>
-              `
-              <div class="grid-item card" >
-                  <img src="${img.urls.small}" class="fetch-img" />
-                  <div class="details">
-                    <div class="icons top-icons">
-                      <button class="share-btn">
-                        <i class="uil uil-share-alt"></i>
-                      </button>
-                    </div>
-                      <div class="photographer">
-                          <i class="uil uil-camera"></i>
-                          <span>${img.user.name}</span>
-                      </div>
-                      <div class="">
-                      <button class="download-btn" onclick="downloadImg('${img.urls.small}');" >
-                        <i class="uil uil-import"></i>
-                      </button>
-                    </div>
-                  </div>
-              </div>
-            `
-          )
-          .join("");
+      const handleScroll = () => {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+          loadImages();
+          currentPage++;
+        }
       };
-
+      
+      window.addEventListener("scroll", handleScroll);
     });
 
     slide.appendChild(heading);
@@ -90,18 +146,15 @@ function displaySlides() {
   });
 }
 
+
+
 // Call the displaySlides function to initially display the slides
 displaySlides();
 
-
-function prev(){
-    document.getElementById('slider-container').scrollLeft -= 270;
+function prev() {
+  document.getElementById("slider-container").scrollLeft -= 270;
 }
 
-function next()
-{
-    document.getElementById('slider-container').scrollLeft += 270;
+function next() {
+  document.getElementById("slider-container").scrollLeft += 270;
 }
-
-
-
