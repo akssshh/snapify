514594a045fd66d4a80afd9d88ea734198bc3704
Akash Gogoi <gogoiakash09@gmail.com>
2024-02-26 12:56:29 +0530

    refactor code

--- scripts/index.js
+++ scripts/index.js
@@ -33,65 +33,6 @@ const fixStartUpBug = () => {
   }, true);
 };
 
-// function to display slider
-
-const sliderContainer = document.getElementById("slider-container");
-
-const titles = [
-  "Cool Tones",
-  "Wallpapers",
-  "Nature",
-  "3D Renders",
-  "Travel",
-  "Architecture & Interiors",
-  "Textures & Patterns",
-  "Street Photography",
-  "Film",
-  "Archival",
-  "Experimental",
-  "Animals",
-  "Fashion & Beauty",
-  "People",
-  "Spirituality",
-  "Business & Work",
-  "Food & Drink",
-  "Health & Wellness",
-  "Sports",
-  "Current Events",
-];
-
-// Function to display the slides
-function displaySlides() {
-  // Clear the slider container
-  sliderContainer.innerHTML = "";
-
-  // Iterate over the names array and create a slide for each
-  titles.forEach((title) => {
-    const slide = document.createElement("div");
-    slide.classList.add("slide");
-    const heading = document.createElement("h4");
-    heading.textContent = title;
-
-    slide.addEventListener("click", () => {
-      currentPage = 1;
-      loadImages(title);
-    });
-
-    slide.appendChild(heading);
-    sliderContainer.appendChild(slide);
-  });
-}
-
-// Call the displaySlides function to initially display the slides
-displaySlides();
-
-function prev() {
-  document.getElementById("slider-container").scrollLeft -= 270;
-}
-
-function next() {
-  document.getElementById("slider-container").scrollLeft += 270;
-}
 
 // function to download image
 
@@ -205,11 +146,8 @@ searchButton.addEventListener("click", () => {
 loadImages();
 
 const handleScroll = () => {
-  if (
-    window.innerHeight + window.scrollY >= document.body.offsetHeight &&
-    searchTerm // Ensure searchTerm is not null
-  ) {
-    loadImages(); // Fetch more images based on the current search term
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    loadImages();
     currentPage++;
   }
 };
